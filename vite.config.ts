903f44839e51044cce93f7158a1392ea869d3df3
Afc_Adinho <afcadinho@gmail.com>
2025-11-08 17:12:35 +0100

    Add base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/import_website/', // 👈 important for GitHub Pages
   plugins: [
     vue(),
     vueDevTools(),
